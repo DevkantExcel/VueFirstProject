50fa117d320badc378f900291cfadacc44a8e5db
etech <devkant@excellencetechnologies.in>
2018-10-15 16:24:33 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <newcomponent></newcomponent>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import newcomponent from './components/newComponent.vue'
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    newcomponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/newComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hi varun</h1>
+        <h3>Welcome to VueJS</h3>
+        <h2>This page been working fine Yo!!!!!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'newcomponent'
+}
+</script>
