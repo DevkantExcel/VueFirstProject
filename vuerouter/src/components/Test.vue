ed2bf54e4ad6d09819dda642338a53ddbcbfad86
etech <devkant@excellencetechnologies.in>
2018-11-01 14:05:39 +0530

    Vue Router Button

---
+++ vuerouter/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+         Testing
+         <button @click="click">Please Click Here</button>
+    </div>
+</template>
+<script>
+export default {
+  name : "Test",
+  methods: {
+    click: function() {
+        this.$router.push('TestNew')
+    }
+  }
+}
+</script>
+<style>
+</style>
---
+++ vuerouter/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+// import store from './store'
+import Test from './components/Test.vue';
+import TestNew from './components/TestNew.vue';
+Vue.use(VueRouter)
+
+
+Vue.config.productionTip = false
+
+//code for router
+const routes = [
+  {path: "/", component: Test},
+  {path: '/TestNew',component: TestNew}
+]
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+
+new Vue({
+  router: router,
+  render: h => h(App),
+  // store,
+}).$mount('#app')
