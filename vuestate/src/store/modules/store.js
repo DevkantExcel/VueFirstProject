11b7fcf60bb57a49027fe7d5a1ad1474c47eb2d6
etech <devkant@excellencetechnologies.in>
2018-11-12 16:15:11 +0530

    delete edit checkbox vuex statepersistance

--- vuestate/src/store/modules/store.js
+++ vuestate/src/store/modules/store.js
@@ -1,18 +1,22 @@
+import Vue from "vue";
+
 export default {
     state: {
         todos: [],
-
     },
     mutations: {
         addtodo: (state,item) =>{
-            // console.log(item)
-            state.todos.push(item)
-            console.log(state.todos)
+            // console.log(item.key01)
+            state.todos.push({'item':item.key00, 'active':item.key01, 'sno':item.key02})
+            // console.log(state.todos)
         },
         delItem: (state, index) => {
             state.todos.splice(index, 1)
+        },
+        editItem: (state, val) => {
+            // console.log(state.todos, val.key1, val.key2)
+            Vue.set(state.todos, val.key1, {'item':val.key2, 'active': val.key3, 'sno': val.key4})
         }
-
     },
     getters: {
         showTodoStore: state => state.todos
@@ -24,6 +28,10 @@ export default {
         },
         delete({commit}, payload) {
             commit('delItem', payload)
+        },
+        edit ({commit}, payload) {
+            // console.log(payload)
+            commit('editItem', payload)
         }
     }
 }
